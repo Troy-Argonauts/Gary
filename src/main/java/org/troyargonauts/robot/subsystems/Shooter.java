532bb1d0d5a69c4e90a262e69c200a36a8ffcaf8
Gargi Sonawane <67555203+GargiSonawane@users.noreply.github.com>
2024-01-25 19:17:06 -0500

    Added Pid values and made isPidFinished function
    @ninasuttatti

--- src/main/java/org/troyargonauts/robot/subsystems/Shooter.java
+++ src/main/java/org/troyargonauts/robot/subsystems/Shooter.java
@@ -7,11 +7,20 @@ import org.troyargonauts.common.motors.wrappers.LazyTalonFX;
 public class Shooter extends SubsystemBase {
     private LazyTalonFX motor1, motor2;
 
+    double motor1P = 0.3;
+    double motor1I = 0.1;
+    double motor1D = 0;
+    double motor1Target = 0;
+
     public Shooter(int talon){
         motor1 = new LazyTalonFX(9);
         motor2 = new LazyTalonFX(10);
     }
     public void setDesiredTarget(int motorID, double target){
+    }
+
+    public boolean isPidFinished(int motorID){
+        
     }
 
 
