a57e181e0de92588440f741dfee24b86af5461f1
Saikrish Balaji <balaji.saikrish@gmail.com>
2024-01-25 19:00:32 -0500

    Created Intake class

---
+++ src/main/java/org/troyargonauts/robot/subsystems/Intake.java
@@ -0,0 +1,14 @@
+package org.troyargonauts.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.troyargonauts.common.motors.MotorCreation;
+import org.troyargonauts.common.motors.wrappers.LazyCANSparkMax;
+import org.troyargonauts.robot.Constants;
+
+public class Intake extends SubsystemBase {
+
+}
