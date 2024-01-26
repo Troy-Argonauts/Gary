cc5fdb0c94fff53a3acb43d274196d5f766bbf0f
ASH-will-WIN <ashwinshrivastav4@gmail.com>
2024-01-25 20:18:59 -0500

    Added other methods
    Added the Brake modes
    added setDesiredState();
    added isPIDFinished
    added Periodic
    
    Co-Authored-By: vishnujaliparhti <147113501+vishnujaliparhti@users.noreply.github.com>
    Co-Authored-By: Sadhanac123 <149122607+Sadhanac123@users.noreply.github.com>

--- src/main/java/org/troyargonauts/robot/subsystems/Arm.java
+++ src/main/java/org/troyargonauts/robot/subsystems/Arm.java
@@ -1,6 +1,8 @@
 package org.troyargonauts.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.troyargonauts.common.motors.wrappers.LazyTalonFX;
 
@@ -9,10 +11,13 @@ public class Arm extends SubsystemBase {
     private LazyTalonFX rightArmMotor;
     private LazyTalonFX leftArmMotor;
     private PIDController armPIDController;
+
+    private double armPosition;
     public Arm(){
-        rightArmMotor = new LazyTalonFX();
-        leftArmMotor = new LazyTalonFX();
-        armPIDController = new PIDController();
+        rightArmMotor = new TalonFX(Constants.Arm.PORT);
+        leftArmMotor = new TalonFX(Constants.Arm.PORT);
+        armPIDController = new PIDController(Constants.Arm.D, Constants.Arm.D, Constants.Arm.D);
+        armPosition = 0;
     }
 
     public void resetEncoders(){
@@ -20,5 +25,24 @@ public class Arm extends SubsystemBase {
         leftArmMotor.setEncoderCounts(0);
     }
 
-    
+    public void setDesiredState(double position) {
+        rightArmMotor.set(armPIDController.calculate(rightArmMotor.getPositionMeters(), position));
+        leftArmMotor.set(armPIDController.calculate(leftArmMotor.getPositionMeters(), position));
+    }
+
+    public boolean isPIDFinished(double position){
+        if (Math.abs(armPosition - position) < 5){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+        @Override
+        public void periodic() {
+            armPosition = (leftArmMotor.getPositionRotations() + rightArmMotor.getPositionRotations())/2;
+
+            SmartDashboard.putNumber("Arm Position:", armPosition);
+        }
 }
