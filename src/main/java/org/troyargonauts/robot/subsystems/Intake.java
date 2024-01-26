5b6a2d5f1854a9817cf02b8a0ec5dc8321d4481d
Saikrish Balaji <balaji.saikrish@gmail.com>
2024-01-25 19:59:17 -0500

    Made Intake Subsystem ready
    Co-Authored-By: firearcher2012 <116320394+firearcher2012@users.noreply.github.com>
    Co-Authored-By: ShivankJ09 <149122594+ShivankJ09@users.noreply.github.com>

--- src/main/java/org/troyargonauts/robot/subsystems/Intake.java
+++ src/main/java/org/troyargonauts/robot/subsystems/Intake.java
@@ -1,5 +1,7 @@
 package org.troyargonauts.robot.subsystems;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -10,5 +12,84 @@ import org.troyargonauts.common.motors.wrappers.LazyCANSparkMax;
 import org.troyargonauts.robot.Constants;
 
 public class Intake extends SubsystemBase {
+    private TalonFX motor1;
+    private DigitalInput noteSensor;
+    public double motor1Encoder;
+    public double motor2Encoder;
+    public double motor3Encoder;
+
+
+
+    double motor1Target = 0.0;
+    final VelocityVoltage velocityVoltage = new VelocityVoltage(0).withSlot(0);
+
+
+
+    public Intake() {
+        motor1 = new TalonFX(1);
+        noteSensor = new DigitalInput(2);
+
+        SmartDashboard.putNumber("motor1SetPoint", 0);
+        SmartDashboard.putNumber("motor2SetPoint", 0);
+        SmartDashboard.putNumber("motor3SetPoint", 0);
+    }
+
+    @Override
+    public void periodic() {
+
+        motor1Encoder = motor1.getVelocity().getValueAsDouble();
+//        motor3Encoder = motor3.getVelocity().getValueAsDouble();
+
+        motor1Target = SmartDashboard.getNumber("motor1SetPoint",0);
+
+        SmartDashboard.putNumber("Motor1Error", (motor1Target - motor1.getVelocity().getValueAsDouble()) * 60);
+
+        SmartDashboard.putNumber("Motor1RPM", (motor1.getVelocity().getValueAsDouble()) * 60);
+    }
+
+
+
+
+    public boolean isNoteReady() {
+        if (noteSensor.get()) {
+            return true;
+        }
+        else{
+            return false;
+        }
+
+
+    }
+    public void setRawPower(int motorID, double speed) {
+
+        if (motorID == 1) {
+            motor1.set(speed);
+        }
+    }
+    enum state{
+        IN,
+        OFF,
+        OUT
+    }
+   state noteState;
+
+    public void setState(){
+
+        switch (noteState){
+            case IN:
+                setRawPower(1,-0.3);
+                break;
+
+            case OFF:
+                setRawPower(1,0);
+                break;
+
+            case OUT:
+                setRawPower(1,0.3);
+                break;
+        }
+
+
+    }
 
 }
