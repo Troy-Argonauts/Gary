89897ce7f467296cfdb75f3b56cba211c0420f24
aarooshg <116320299+aarooshg@users.noreply.github.com>
2024-01-25 19:08:34 -0500

    Instantiated motors
    Added motors and common code so others could work on it

--- src/main/java/org/troyargonauts/robot/subsystems/Shooter.java
+++ src/main/java/org/troyargonauts/robot/subsystems/Shooter.java
@@ -1,8 +1,18 @@
 package org.troyargonauts.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.troyargonauts.common.motors.wrappers.LazyTalonFX;
 
 public class Shooter extends SubsystemBase {
-    
+    private LazyTalonFX motor1, motor2;
+
+    public Shooter(int talon){
+        motor1 = new LazyTalonFX(9);
+        motor2 = new LazyTalonFX(10);
+    }
+    public void setDesiredTarget(int motorID, double target){
+    }
+
 
 }
