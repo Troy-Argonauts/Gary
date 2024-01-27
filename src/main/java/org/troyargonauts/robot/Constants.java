e655f042c826880062317eb1f9764b3cbf27f2ac
ASH-will-WIN <ashwinshrivastav4@gmail.com>
2024-01-27 16:35:11 -0500

    Added canivore variable
    added canivore variable for climber
    
    Co-Authored-By: Nihaar <67766312+Nihaar57@users.noreply.github.com>

--- src/main/java/org/troyargonauts/robot/Constants.java
+++ src/main/java/org/troyargonauts/robot/Constants.java
@@ -8,6 +8,7 @@ public final class Constants {
     public interface Intake{
         int MOTOR_CAN_ID = 1;
         int NOTE_SENSOR_SLOT = 2;
+        String INTAKE_CANBUS_NAME = "roborio";
     }
 
     public interface Controllers {
