938d6bfaf4e4c3664552bd0c3b1d3491f5260637
Nihaar <njampala25@gmail.com>
2024-02-03 17:46:01 -0500

    Update Climber.java

--- src/main/java/org/troyargonauts/robot/subsystems/Climber.java
+++ src/main/java/org/troyargonauts/robot/subsystems/Climber.java
@@ -1,8 +1,11 @@
 package org.troyargonauts.robot.subsystems;
 
 
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.util.datalog.DoubleLogEntry;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static org.troyargonauts.robot.Constants.Climber.*;
@@ -18,8 +21,18 @@ public class Climber extends SubsystemBase {
     /**
      * Creates motorController object.
      */
-
+    private final PositionVoltage positionVoltage = new PositionVoltage(0).withSlot(0);
     private TalonFX climberMotor;
+
+    private double climberEncoder = 0;
+
+    private double climberTarget = 0;
+
+    private DoubleLogEntry climberEncoderLog;
+    private DoubleLogEntry climberOutputCurrentLog;
+    private DoubleLogEntry climberBusVoltage;
+    private DoubleLogEntry climberTargetLog;
+
 
     /**
      * Instantiates motorController object and sets the neutral mode to break.
@@ -28,14 +41,37 @@ public class Climber extends SubsystemBase {
     public Climber(){
         climberMotor = new TalonFX(CLIMBER_MOTOR_PORT);
         climberMotor.setNeutralMode(NeutralModeValue.Brake);
+
+        climberEncoder = climberMotor.getPosition().getValueAsDouble();
+
+        SmartDashboard.putNumber("Climber Encoder: ", climberEncoder);
+
+        climberEncoderLog.append(climberEncoder);
+        climberOutputCurrentLog.append(climberMotor.getSupplyCurrent().getValue());
+        climberBusVoltage.append(climberMotor.getMotorVoltage().getValue());
+        climberTargetLog.append(climberEncoder);
+    }
+
+    public void resetEncoders() {
+        climberMotor.setPosition(0);
     }
 
     /**
      * Creates 3 different motor states (OFF, DOWN, and UP).
      */
 
+
+    public void run(double target) {
+        climberMotor.setControl(positionVoltage.withPosition(target));
+    }
+
     public enum MotorStates {
         UP, OFF
+    }
+
+    public boolean isPidFinished(int motorID) {
+        return (Math.abs((climberTarget - climberMotor.getVelocity().getValueAsDouble())) <= 5);
+
     }
 
     /**
