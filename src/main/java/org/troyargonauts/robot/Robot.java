e8ded5992bfec8efb41384f56882cb1c65596046
Argonauts Driver Station <101407414+ArgonautsStation@users.noreply.github.com>
2024-02-13 18:45:41 -0500

    Generated Swerve Code
    Created drivetrain branch for 2024 season code.
    
    Co-Authored-By: rohangajulaghosh <40278130+SolidityContract@users.noreply.github.com>
    Co-Authored-By: Sharvayu Chavan <69215325+Sharvayu-Chavan@users.noreply.github.com>
    Co-Authored-By: Ashwin Shrivastav <103900575+ASH-will-WIN@users.noreply.github.com>

--- src/main/java/org/troyargonauts/robot/Robot.java
+++ src/main/java/org/troyargonauts/robot/Robot.java
@@ -1,80 +1,76 @@
 // Copyright (c) FIRST and other WPILib contributors.
-
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package org.troyargonauts.robot;
+package frc.robot;
 
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import org.troyargonauts.robot.subsystems.*;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
-/**
- * The VM is configured to automatically run this class, and to call the methods corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-    private final SendableChooser<Command> chooser = new SendableChooser<>();
-    private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-    private Command autonomousCommand;
+  private Command m_autonomousCommand;
 
+  private RobotContainer m_robotContainer;
 
-    @Override
-    public void robotInit() {
-        LiveWindow.disableAllTelemetry();
-        LiveWindow.setEnabled(false);
+  @Override
+  public void robotInit() {
+    m_robotContainer = new RobotContainer();
+  }
 
-        DataLogManager.start("/media/sda1/logs");
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run(); 
+  }
 
-        new RobotContainer();
+  @Override
+  public void disabledInit() {}
 
-        CameraServer.startAutomaticCapture().setFPS(14);
+  @Override
+  public void disabledPeriodic() {}
 
-        SmartDashboard.putData("Autonomous modes", chooser);
-        chooser.addOption("Nothing", new WaitCommand(15));
+  @Override
+  public void disabledExit() {}
+
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
     }
+  }
 
-    @Override
-    public void robotPeriodic() {
-        CommandScheduler.getInstance().run();
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void autonomousExit() {}
+
+  @Override
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
     }
+  }
 
-    @Override
-    public void disabledInit() {
-    }
+  @Override
+  public void teleopPeriodic() {}
 
-    @Override
-    public void autonomousInit() {
-        autonomousCommand = chooser.getSelected();
-        if (autonomousCommand != null) {
-            autonomousCommand.schedule();
-        }
-    }
+  @Override
+  public void teleopExit() {}
 
-    @Override
-    public void teleopInit() {
-        if (autonomousCommand != null) {
-            autonomousCommand.cancel();
-        }
-    }
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
 
-    @Override
-    public void testInit() {
-        // Cancels all running commands at the start of test mode.
-        CommandScheduler.getInstance().cancelAll();
-    }
+  @Override
+  public void testPeriodic() {}
 
+  @Override
+  public void testExit() {}
+
+  @Override
+  public void simulationPeriodic() {}
 }
--- src/main/java/org/troyargonauts/robot/RobotContainer.java
+++ src/main/java/org/troyargonauts/robot/RobotContainer.java
@@ -1,65 +1,63 @@
 // Copyright (c) FIRST and other WPILib contributors.
-
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package org.troyargonauts.robot;
+package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import org.troyargonauts.common.input.Gamepad;
-import org.troyargonauts.common.input.gamepads.AutoGamepad;
+import com.ctre.phoenix6.Utils;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.generated.TunerConstants;
+
 public class RobotContainer {
+  private double MaxSpeed = 6; // 6 meters per second desired top speed
+  private double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
 
-    public static final Gamepad driver = new AutoGamepad(Constants.Controllers.DRIVER);
-    public static final Gamepad operator = new AutoGamepad(Constants.Controllers.OPERATOR);
+  /* Setting up bindings for necessary control of the swerve drive platform */
+  private final CommandXboxController joystick = new CommandXboxController(0); // My joystick
+  private final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain; // My drivetrain
 
-    public RobotContainer() {
-        // Configure the button bindings
-        configureBindings();
+  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+      .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+      .withDriveRequestType(DriveRequestType.Velocity); // I want field-centric
+                                                               // driving in open loop
+  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+  private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+  private final Telemetry logger = new Telemetry(MaxSpeed);
+
+  private void configureBindings() {
+    drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
+        drivetrain.applyRequest(() -> drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with
+                                                                                           // negative Y (forward)
+            .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+            .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+        ));
+
+    joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+    joystick.b().whileTrue(drivetrain
+        .applyRequest(() -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
+
+    // reset the field-centric heading on left bumper press
+    joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
+
+    if (Utils.isSimulation()) {
+      drivetrain.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
     }
+    drivetrain.registerTelemetry(logger::telemeterize);
+  }
 
-    /**
-     * Use this method to define your controller->command mappings.
-     */
-    private void configureBindings() {
-        //Example Joystick Input Command
-        //Pulled for Troy-Argonauts/Butters
-//            Robot.getDrivetrain().setDefaultCommand(
-//                    new RunCommand(
-//                            () -> {
-//                                double speed = IStream.create(driver::getLeftY)
-//                                        .filtered(x -> OMath.deadband(x, Constants.DriveTrain.DEADBAND))
-//                                        .get();
-//                                double angle = IStream.create(driver::getRightX)
-//                                        .filtered(x -> OMath.deadband(x, Constants.DriveTrain.DEADBAND))
-//                                        .get();
-//                                Robot.getDrivetrain().cheesyDrive(speed, angle, 1);
-//                            }, Robot.getDrivetrain()
-//                    )
-//            );
+  public RobotContainer() {
+    configureBindings();
+  }
 
-        //Example Button Input Command
-        //Pulled for Troy-Argonauts/Butters
-//        driver.getRightBumper().whileTrue(
-//                new InstantCommand(() -> Robot.getDrivetrain().getDualSpeedTransmission().disableAutomaticShifting())
-//                        .andThen(new InstantCommand(() -> getDriver().setRumble(1.0, 0.5)))
-//        );
-    }
-
-    public static Gamepad getDriver() {
-        return driver;
-    }
-
-    public static Gamepad getOperator() {
-        return operator;
-    }
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
 }
